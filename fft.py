7ebcb8914afbec96db9eefac4a59d462a9817b50
Pablo Pino <pdpino@uc.cl>
2017-06-04 23:59:15 -0400

    use workshop method to compute fft, get alpha, beta, gamma, theta and delta (are they ok?)

--- fft.py
+++ fft.py
@@ -2,257 +2,145 @@
 """Script that analyses data with a fourier transform"""
 
 import numpy as np
-# import mne
 import pandas as pd
 import matplotlib.pyplot as plt
 import argparse
 
-def using_np():
-    def create_parser():
-        """Create the console arguments parser"""
-        parser = argparse.ArgumentParser(description='Apply Fourier transformations and plot', usage='%(prog)s [options]')
 
-        parser.add_argument('-c', '--cols', nargs='+',
-                            help="Columns to apply the transformation")
-        parser.add_argument('-f', '--filename', default="dump", type=str,
-                            help="Name of the csv to load")
-        parser.add_argument('-d', '--dir', default="data", type=str,
-                            help="Folder of the csv")
-        parser.add_argument('-s', '--slice', default=None, type=int,
-                            help="First n numbers to grab")
+"""Basado en codigo de: https://github.com/NeuroTechX/bci-workshop"""
 
+def compute_feature_vector(eegdata, Fs):
+    """Extract the features from the EEG
+    Inputs:
+    eegdata: array of dimension [number of samples, number of channels]
+    Fs: sampling frequency of eegdata
 
-        return parser
+    Outputs:
+    feature_vector: [number of features points; number of different features]
 
-    def get_fourier(df, col, sl=None):
-        arr = df[col].as_matrix()
-        if not sl is None:
-            arr = arr[:sl]
-        return abs(np.fft.fft(arr))
 
-    def plot_fourier(f, col, show=False):
-        x = range(len(f))
-        plt.plot(x, f)
-        plt.title(col)
+    """
 
-        if show:
-            plt.show()
+    n_samples, n_channels = eegdata.shape
 
-    parser = create_parser()
-    args = parser.parse_args()
+    ### 1. Compute the PSD # Power Spectral Density
 
-    filename = args.dir + "/" + args.filename
-    ext = ".csv"
-    if not filename.endswith(ext):
-        filename += ext
+    # Apply Hamming window
+    w = np.hamming(n_samples)
 
-    df = pd.read_csv(filename, index_col=0)
+    print(w)
+    input()
 
-    if args.cols is None:
-        cols = list(df.columns)
-    else:
-        cols = args.cols
+    dataWinCentered = eegdata - np.mean(eegdata, axis=0) # Remove offset
+    dataWinCenteredHam = (dataWinCentered.T*w).T
 
-    i = 1
-    for c in cols:
-        f = get_fourier(df, c, sl=args.slice)
-        plt.subplot(2, 3, i)#"22{}".format(i))
+    NFFT = nextpow2(n_samples)
+    Y = np.fft.fft(dataWinCenteredHam, n=NFFT, axis=0)/n_samples
+    a = int(NFFT/2)
+    PSD = 2*np.abs(Y[0:a,:])
+    f = Fs/2*np.linspace(0,1,a)
 
-        i += 1
 
-        plot_fourier(f, c)
+    # SPECTRAL FEATURES
+    # Average of band powers
+    # Delta <4
+    ind_delta, = np.where(f<4)
+    meanDelta = np.mean(PSD[ind_delta,:],axis=0)
+    # Theta 4-8
+    ind_theta, = np.where((f>=4) & (f<=8))
+    meanTheta = np.mean(PSD[ind_theta,:],axis=0)
+    # Alpha 8 - 12
+    ind_alpha, = np.where((f>=8) & (f<=12))
+    meanAlpha = np.mean(PSD[ind_alpha,:],axis=0)
+    # Beta 12-30
+    ind_beta, = np.where((f>=12) & (f<=30))
+    meanBeta = np.mean(PSD[ind_beta,:],axis=0)
+    # Gamma 30>
+    ind_gamma, = np.where(f>=30)
+    meanGamma = np.mean(PSD[ind_gamma,:],axis=0)
 
-    plt.show()
 
-def using_mne():
-    df = pd.read_csv("data/data.csv", index_col=0)
+    feature_vector = np.concatenate((meanDelta, meanTheta, meanAlpha, meanBeta, meanGamma),axis=0)
 
-    # Some information about the channels
-    ch_names = ['TP9', 'AF7', 'AF8', 'TP10']
-    ch_types = ['eeg', 'eeg', 'eeg', 'eeg']
+    feature_vector = np.log10(feature_vector)
 
-    df = df[ch_names]
+    return feature_vector
 
-    # Sampling rate
-    sfreq = 500  # Hz
+def compute_feature_vector_one(eegdata, Fs):
+    """Extract the features from the EEG, only with one axis
+    Inputs:
+    eegdata: array of dimension [number of samples]
+    Fs: sampling frequency of eegdata
 
-    # Create the info structure needed by MNE
-    info = mne.create_info(ch_names=ch_names, ch_types=ch_types, sfreq=sfreq)
+    Outputs:
+    feature_vector: [number of features points; number of different features]
 
+    """
 
-    # Tomar datos
-    data = df.as_matrix().transpose()
+    n_samples = len(eegdata)
 
-    # Finally, create the Raw object
-    raw = mne.io.RawArray(data, info)
+    ### 1. Compute the PSD # Power Spectral Density
 
-    # raw.plot_psd(tmax=np.inf)
+    ## Apply Hamming window
+    w = np.hamming(n_samples)
+    dataWinCentered = eegdata - np.mean(eegdata) # Remove offset # QUESTION: pq restar el promedio?
+    dataWinCenteredHam = (dataWinCentered.T*w).T
 
-    def plot_wave(fmin, fmax, title, i):
-        alpha = raw.filter(fmin,fmax, method='iir')
-        a, t = alpha.get_data(start=1000, return_times=True)
-        tp9 = a[0]
-        af7 = a[1]
-        af8 = a[2]
-        tp10 = a[3]
+    ## FFT
+    Y = np.fft.fft(dataWinCenteredHam) #, n=NFFT) #, axis=0)
+    Y = Y/n_samples ## QUESTION: pq esto?
 
-        plt.subplot(3, 2, i)
-        plt.plot(t, tp9 + af7 + af8 + tp10)
-        plt.title(title)
-        # plt.show()
+    ## Tomar arreglo de frecuencias
+    n_freqs = int(n_samples/2)
+    PSD = 2*np.abs(Y[0:n_freqs]) # Transformar numero complejo a real # QUESTION: pq *2 ??
+    f = Fs/2*np.linspace(0,1,n_freqs) # Tomar rango de frecuencias
 
-    plot_wave(1, 4, "Delta", 1)
-    plot_wave(4, 18, "Theta", 2)
-    plot_wave(8, 13, "Alpha", 3)
-    plot_wave(13, 30, "Beta", 4)
-    plot_wave(30, 44, "Gamma", 5)
 
-    plt.show()
+    ## Filtrar por frecuencias
+    # Delta < 4
+    ind_delta, = np.where(f<4)
+    delta = np.sum(PSD[ind_delta])
 
-def using_workshop():
-    def nextpow2(i):
-        """ Find the next power of 2 for number i """
-        n = 1
-        while n < i:
-            n *= 2
-        return n
+    # Theta 4-8
+    ind_theta, = np.where((f>=4) & (f<=8))
+    theta = np.sum(PSD[ind_theta])
 
-    def compute_feature_vector(eegdata, Fs):
-        """Extract the features from the EEG
-        Inputs:
-        eegdata: array of dimension [number of samples, number of channels]
-        Fs: sampling frequency of eegdata
+    # Alpha 8 - 12
+    ind_alpha, = np.where((f>=8) & (f<=12))
+    alpha = np.sum(PSD[ind_alpha])
 
-        Outputs:
-        feature_vector: [number of features points; number of different features]
+    # Beta 12-30
+    ind_beta, = np.where((f>=12) & (f<=30))
+    beta = np.sum(PSD[ind_beta])
 
-        """
+    # Gamma 30>
+    ind_gamma, = np.where((f>=30) & (f<=44))
+    gamma = np.sum(PSD[ind_gamma])
 
-        n_samples, n_channels = eegdata.shape
 
-        ### 1. Compute the PSD # Power Spectral Density
+    feature_vector = np.array([delta, theta, alpha, beta, gamma])
 
-        # Apply Hamming window
-        w = np.hamming(n_samples)
 
-        print(w)
-        input()
+    good = True
 
-        dataWinCentered = eegdata - np.mean(eegdata, axis=0) # Remove offset
-        dataWinCenteredHam = (dataWinCentered.T*w).T
+    # Revisar si son 0, log tira error
+    if beta == 0 or delta == 0 or theta == 0 or alpha == 0 or gamma == 0:
+        good = False
 
-        NFFT = nextpow2(n_samples)
-        Y = np.fft.fft(dataWinCenteredHam, n=NFFT, axis=0)/n_samples
-        a = int(NFFT/2)
-        PSD = 2*np.abs(Y[0:a,:])
-        f = Fs/2*np.linspace(0,1,a)
+    feature_vector = np.log10(feature_vector)
 
+    return list(feature_vector), good
 
-        # SPECTRAL FEATURES
-        # Average of band powers
-        # Delta <4
-        ind_delta, = np.where(f<4)
-        meanDelta = np.mean(PSD[ind_delta,:],axis=0)
-        # Theta 4-8
-        ind_theta, = np.where((f>=4) & (f<=8))
-        meanTheta = np.mean(PSD[ind_theta,:],axis=0)
-        # Alpha 8 - 12
-        ind_alpha, = np.where((f>=8) & (f<=12))
-        meanAlpha = np.mean(PSD[ind_alpha,:],axis=0)
-        # Beta 12-30
-        ind_beta, = np.where((f>=12) & (f<=30))
-        meanBeta = np.mean(PSD[ind_beta,:],axis=0)
-        # Gamma 30>
-        ind_gamma, = np.where(f>=30)
-        meanGamma = np.mean(PSD[ind_gamma,:],axis=0)
-
-
-        feature_vector = np.concatenate((meanDelta, meanTheta, meanAlpha, meanBeta, meanGamma),axis=0)
-
-        feature_vector = np.log10(feature_vector)
-
-        return feature_vector
-
-    sd = []
-
-    def compute_feature_vector_one(eegdata, Fs):
-        """Extract the features from the EEG, only with one axis
-        Inputs:
-        eegdata: array of dimension [number of samples]
-        Fs: sampling frequency of eegdata
-
-        Outputs:
-        feature_vector: [number of features points; number of different features]
-
-        """
-
-        n_samples = len(eegdata)
-
-        ### 1. Compute the PSD # Power Spectral Density
-
-        # Apply Hamming window
-        w = np.hamming(n_samples)
-
-        dataWinCentered = eegdata# - np.mean(eegdata) # Remove offset
-        dataWinCenteredHam = (dataWinCentered.T*w).T
-
-        Y = np.fft.fft(dataWinCenteredHam) #, n=NFFT) #, axis=0)
-        Y = Y/n_samples
-
-        n_freqs = int(n_samples/2)
-
-        PSD = 2*np.abs(Y[0:n_freqs]) # Transformar numero complejo a real # QUESTION: pq *2 ??
-        f = Fs/2*np.linspace(0,1,n_freqs) # Tomar rango de frecuencias
-
-        # SPECTRAL FEATURES
-        # Delta < 4
-        ind_delta, = np.where(f<4)
-        delta = np.sum(PSD[ind_delta])
-
-        # Theta 4-8
-        ind_theta, = np.where((f>=4) & (f<=8))
-        theta = np.sum(PSD[ind_theta])
-
-        # Alpha 8 - 12
-        ind_alpha, = np.where((f>=8) & (f<=12))
-        alpha = np.sum(PSD[ind_alpha])
-
-        # Beta 12-30
-        ind_beta, = np.where((f>=12) & (f<=30))
-        beta = np.sum(PSD[ind_beta])
-
-        # Gamma 30>
-        ind_gamma, = np.where((f>=30) & (f<=44))
-        gamma = np.sum(PSD[ind_gamma])
-
-
-        feature_vector = np.array([delta, theta, alpha, beta, gamma])
-
-        # Revisar si son 0, log tira error
-        if beta == 0 or delta == 0 or theta == 0 or alpha == 0 or gamma == 0:
-            print(feature_vector)
-            print(eegdata)
-            print(i)
-
-
-        feature_vector = np.log10(feature_vector)
-        sd.append(np.std(feature_vector))
-
-        return list(feature_vector)
-
-
+if __name__ == "__main__":
 
     df = pd.read_csv("data/data.csv", index_col=0)
 
     # Some information about the channels
     ch_names = ['TP9', 'AF7', 'AF8', 'TP10']
-
-
 
     # Tomar data
     # data = df[ch_names].as_matrix()
-    a = df['AF7'].as_matrix()
-
+    a = df['TP10'].as_matrix()
 
     # Sampling rate
     sample_rate = 256  # Hz
@@ -261,18 +149,20 @@ def using_workshop():
     feats = []
     i = 0
     n = len(a)
+    contador_malos = 0
     while i < n:
         b = a[i:i+window]
         if(len(b) >= window):
-            feats.append(compute_feature_vector_one(b, sample_rate))
+            f, good = compute_feature_vector_one(b, sample_rate)
+            if good:
+                feats.append(f)
+            else:
+                contador_malos += 1
         i += step
 
-    sd = np.array(sd)
-    # print(np.mean(sd))
-
     feats = np.matrix(feats)
-    # print(len(feats))
-    # print(feats)
+
+    print("cantidad de veces con solo 0s: {}".format(contador_malos))
 
     delta = feats[:, 0]
     theta = feats[:, 1]
@@ -280,14 +170,24 @@ def using_workshop():
     beta = feats[:, 3]
     gamma = feats[:, 4]
 
-    def plot_wave(data, title, i):
-        global plot_i
 
+    multiple_graph = False
+    limitx = False
+    limity = False
+
+    def plot_wave(data, title, i):
         t = range(len(data))
-        plt.subplot(3, 2, i)
-        plt.plot(t, data)
-        plt.title(title)
-        # plt.show()
+        if multiple_graph:
+            plt.subplot(3, 2, i)
+            plt.title(title)
+
+        plt.plot(t, data, label=title)
+
+        if limitx:
+            plt.xlim(200, 400)
+
+        if limity:
+            plt.ylim(1, 4)
 
     plot_wave(delta, "Delta", 1)
     plot_wave(theta, "Theta", 2)
@@ -295,13 +195,11 @@ def using_workshop():
     plot_wave(beta, "Beta", 4)
     plot_wave(gamma, "Gamma", 5)
 
+    plt.ylabel("dB")
+    plt.xlabel("Tiempo (no en segundos)")
+    plt.legend()
     plt.show()
 
     # n = len(feats)
     # plt.plot(range(n), feats)
     # plt.show()
-
-
-
-if __name__ == "__main__":
-    using_workshop()
