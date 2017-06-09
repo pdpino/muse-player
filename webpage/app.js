f5eda234b30b86a4c82b983e27e078ffcf122141
Pablo Pino <pdpino@uc.cl>
2017-06-09 11:20:17 -0400

    order some of the app.js code

--- webpage/app.js
+++ webpage/app.js
@@ -2,42 +2,12 @@
 
 // TODO: ordenar codigo
 
-$(document).ready( function() {
-  // Data inicial
-  var n = 100;
-  var data = new Array(n);
-  var segs = 5; //segundos de ventana // HACK
-  for(var i=0;i<n;i++){
-    data[i] = {T: i*segs/n - segs, CH1: 0, CH2: 0, CH3: 0, CH4: 0, CH5: 0};
-  }
 
+function create_graph(segs, yMin, yMax){
   // Margenes
   var margin = {top: 10, right: 10, bottom: 20, left: 40},
       width = 600 - margin.left - margin.right,
       height = 300 - margin.top - margin.bottom;
-
-  // Rangos de ejes
-  var x = d3.scale.linear().domain([-segs, 0]).range([0, width]); // x
-  var y = d3.scale.linear().domain([-1000, 1000]).range([height, 0]); // y // HACK: adaptar eje
-
-  // Funciones para obtener lineas
-  var lineCH1 = d3.svg.line()
-      .x(function(d) { return x(d.T); })
-      .y(function(d) { return y(d.CH1); });
-  var lineCH2 = d3.svg.line()
-      .x(function(d) { return x(d.T); })
-      .y(function(d) { return y(d.CH2); });
-  var lineCH3 = d3.svg.line()
-      .x(function(d) { return x(d.T); })
-      .y(function(d) { return y(d.CH3); });
-  var lineCH4 = d3.svg.line()
-      .x(function(d) { return x(d.T); })
-      .y(function(d) { return y(d.CH4); });
-  var lineCH5 = d3.svg.line()
-      .x(function(d) { return x(d.T); })
-      .y(function(d) { return y(d.CH5); });
-
-
 
   // Svg
   var svg = d3.select("#chart_container").append("svg")
@@ -53,45 +23,89 @@ $(document).ready( function() {
       .attr("width", width)
       .attr("height", height);
 
+  // Rangos de ejes
+  var xRange = d3.scale.linear().domain([-segs, 0]).range([0, width]); // x
+  var yRange = d3.scale.linear().domain([yMin, yMax]).range([height, 0]); // y // HACK: adaptar eje
+
   // Eje del tiempo
-  var xAxis = d3.svg.axis().scale(x).orient("bottom");
+  var xAxis = d3.svg.axis().scale(xRange).orient("bottom");
   svg.append("g")
       .attr("class", "x axis")
       .attr("transform", "translate(0," + height + ")")
       .call(xAxis);
-
   // Y axis
-  var yAxis = d3.svg.axis().scale(y).orient("left").ticks(5);
+  var yAxis = d3.svg.axis().scale(yRange).orient("left").ticks(5);
   svg.append("g")
       .attr("class", "y axis")
       .call(yAxis);
 
+  return {svg: svg, xRange: xRange, yRange: yRange, xAxis: xAxis, yAxis: yAxis};
+}
+
+function create_line_functions(x, y){
+  var lines = Array(5);
+
+  // Funciones para obtener lineas
+  lines[0] = d3.svg.line()
+      .x(function(d) { return x(d.T); })
+      .y(function(d) { return y(d.CH1); });
+  lines[1] = d3.svg.line()
+      .x(function(d) { return x(d.T); })
+      .y(function(d) { return y(d.CH2); });
+  lines[2] = d3.svg.line()
+      .x(function(d) { return x(d.T); })
+      .y(function(d) { return y(d.CH3); });
+  lines[3] = d3.svg.line()
+      .x(function(d) { return x(d.T); })
+      .y(function(d) { return y(d.CH4); });
+  lines[4] = d3.svg.line()
+      .x(function(d) { return x(d.T); })
+      .y(function(d) { return y(d.CH5); });
+
+  return lines;
+};
+
+$(document).ready( function() {
+  // Data
+  var n = 100;
+  var n_channels = 5;
+  var data = new Array(n);
+  var segs = 5; //segundos de ventana // HACK
+  for(var i=0;i<n;i++){
+    data[i] = {T: i*segs/n - segs, CH1: 0, CH2: 0, CH3: 0, CH4: 0, CH5: 0};
+  }
+
+
+  var yMin = -1000;
+  var yMax = 1000;
+
+
+  var colors = ["black", "red", "blue", "green", "cyan"];
+
+  var graph = create_graph(segs, yMin, yMax);
+
+  var lines = create_line_functions(graph.xRange, graph.yRange);
 
   // Crear paths
   function create_path(line, color){
-    return svg.append("g")
+    return graph.svg.append("g")
             .attr("clip-path", "url(#clip)")
           .append("path")
             .attr("class", "line")
             .style("stroke", color)
             .attr("d", line(data));
 
+  };
+
+  var paths = Array(n_channels);
+  for(var i=0;i<n_channels;i++){
+    paths[i] = create_path(lines[i], colors[i]);
   }
 
-  var colors = ["black", "red", "blue", "green", "cyan"];
-
-  var pathCH1 = create_path(lineCH1, colors[0]);
-  var pathCH2 = create_path(lineCH2, colors[1]);
-  var pathCH3 = create_path(lineCH3, colors[2]);
-  var pathCH4 = create_path(lineCH4, colors[3]);
-  var pathCH5 = create_path(lineCH5, colors[4]);
-
-  var plot_ch1 = true; //$("#ch1").checked;
-  var plot_ch2 = true; //$("#ch2").checked;
-  var plot_ch3 = true; //$("#ch3").checked;
-  var plot_ch4 = true; //$("#ch4").checked;
-  var plot_ch5 = true; //$("#ch5").checked;
-
+  var plot_bools = Array(n_channels);
+  for(var i=0;i<n_channels;i++){
+    plot_bools[i] = true;
+  }
 
   function update_line(plot_ch, path, line){
     if(plot_ch){
@@ -105,43 +119,29 @@ $(document).ready( function() {
 
   // Funcion para updatear el grafico
   function update_graph() {
-    update_line(plot_ch1, pathCH1, lineCH1);
-    update_line(plot_ch2, pathCH2, lineCH2);
-    update_line(plot_ch3, pathCH3, lineCH3);
-    update_line(plot_ch4, pathCH4, lineCH4);
-    update_line(plot_ch5, pathCH5, lineCH5);
+    for(var i=0;i<n_channels;i++){
+      update_line(plot_bools[i], paths[i], lines[i]);
+    }
 
     // Setear rango de tiempo de nuevo
-    x.domain(d3.extent(data, function(d) { return d.T; }));
+    graph.xRange.domain(d3.extent(data, function(d) { return d.T; }));
     // y.domain(d3.extent(data, function(d) { return d.Y; }));
     // y.domain([d3.min(data, function(d) { return Math.max(d.Y, -5);}),
     //           d3.max(data, function(d) { return Math.min(d.Y, 5); })]);
     // TODO: scale Y
 
-    svg.select(".x.axis") // change the x axis
-        .call(xAxis);
+    graph.svg.select(".x.axis") // change the x axis
+        .call(graph.xAxis);
   };
 
+  // Pintar leyenda
   d3.select("#ch1-rect").style("fill", colors[0]);
   d3.select("#ch2-rect").style("fill", colors[1]);
   d3.select("#ch3-rect").style("fill", colors[2]);
   d3.select("#ch4-rect").style("fill", colors[3]);
   d3.select("#ch5-rect").style("fill", colors[4]);
 
-  //$("#ch1").textContent = "textContent";
-  // $("#ch1").text("d3");
-  // $("#ch1").value = "value";
-  // $("#ch1").innerText = "innerText";
-  // $("#ch1").innerHTML = "innerHTML";
 
-
-
-  // var legend = svg.append("g")
-  //     .attr("class","legend")
-  //     .attr("transform","translate(50,30)")
-  //     .style("font-size","12px")
-  //     .call(d3.legend);
-  // Legend: ['TP9', 'AF7', 'AF8', 'TP10', 'Right AUX']
 
 
 
@@ -183,24 +183,24 @@ $(document).ready( function() {
 
   // Form de mostrar o no canales
   $("#ch1").click(function(){
-    plot_ch1 = this.checked;
-    toggle_show_path(pathCH1, this.checked);
+    plot_bools[0] = this.checked;
+    toggle_show_path(paths[0], this.checked);
   });
   $("#ch2").click(function(){
-    plot_ch2 = this.checked;
-    toggle_show_path(pathCH2, this.checked);
+    plot_bools[1] = this.checked;
+    toggle_show_path(paths[1], this.checked);
   });
   $("#ch3").click(function(){
-    plot_ch3 = this.checked;
-    toggle_show_path(pathCH3, this.checked);
+    plot_bools[2] = this.checked;
+    toggle_show_path(paths[2], this.checked);
   });
   $("#ch4").click(function(){
-    plot_ch4 = this.checked;
-    toggle_show_path(pathCH4, this.checked);
+    plot_bools[3] = this.checked;
+    toggle_show_path(paths[3], this.checked);
   });
   $("#ch5").click(function(){
-    plot_ch5 = this.checked;
-    toggle_show_path(pathCH5, this.checked);
+    plot_bools[4] = this.checked;
+    toggle_show_path(paths[4], this.checked);
   });
 
   // Boton cerrar conexion
@@ -208,6 +208,12 @@ $(document).ready( function() {
     stream.close();
     console.log("Connection closed");
   });
+
+  // Boton debug
+  $("#debug-btn").click(function(){
+
+  });
+
 
 
   console.log("All set.");
