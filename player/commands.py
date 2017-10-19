67e2cd65291428669c31c91ea9425869fe628069
Pablo Pino <pdpino@uc.cl>
2017-10-18 23:37:24 -0300

    Add help command for the CommandHandler

--- player/commands.py
+++ player/commands.py
@@ -1,10 +1,14 @@
 class CommandHandler:
     """Save the commands that can be used during the run of the player."""
 
-    def __init__(self):
+    def __init__(self, use_help=True):
         self.saved_commands = dict()
+        self.commands_help = dict()
 
-    def add_command(self, command, action, notification, return_value):
+        if use_help:
+            self.add_help_command()
+
+    def add_command(self, command, action, notification, return_value, cmd_help=None):
         """Add a known command to the handler.
 
         If return value is:
@@ -13,6 +17,12 @@ class CommandHandler:
         'notification' -- return the notification
         """
         self.saved_commands[command] = [action, notification, return_value]
+
+        if not cmd_help is None:
+            self.commands_help[command] = str(cmd_help)
+
+    def add_help_command(self):
+        self.add_command("-h", self.show_help, None, None, "Show this help message")
 
     def exist_command(self, command):
         return command in self.saved_commands
@@ -34,3 +44,8 @@ class CommandHandler:
 
         # Return the value get with return_value, and the default (in case not found) is return value
         return possible_returns.get(return_value, return_value)
+
+    def show_help(self):
+        for cmd in self.saved_commands:
+            cmd_help = self.commands_help.get(cmd, "")
+            print("'{}' -- {}".format(cmd, cmd_help))
